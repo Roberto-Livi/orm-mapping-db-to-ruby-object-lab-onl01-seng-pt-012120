defd1f487cd689768c42827264a54b21aeb35fea
Roberto Livi <robertomlivi@outlook.com>
2020-03-21 20:21:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -76,9 +76,9 @@ class Student
     end
   end
   
-  def self.first_X_students_in_grade_10
+  def self.first_X_students_in_grade_10(num)
     sql = <<-SQL
-    SELECT 
+    SELECT * FROM students WHERE grade = 10 LIMIT num;
     SQL
   end
   
